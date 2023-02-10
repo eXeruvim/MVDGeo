1c041fdee864f9b35712caea05443973c68666ab
Andrey <exeruvim@gmail.com>
2023-02-10 21:40:35 +0300

    Initial commit

---
+++ src/main/java/org/geomvd/mvdgeo/MvdGeoApplication.java
@@ -0,0 +1,13 @@
+package org.geomvd.mvdgeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvdGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvdGeoApplication.class, args);
+	}
+
+}
